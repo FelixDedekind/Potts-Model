17892d6a1c20de95e38f221194e905ccf0d8132a
FelixDedekind <png.spammail@gmail.com>
2023-12-16 10:58:48 +0100

    created some new files

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "vars.h"
+
+//compile with gcc -o pottsmodel main.c vars.c -lm
+
+int main() {
+    return 0;
+}
---
+++ vars.c
@@ -0,0 +1,10 @@
+#include "vars.h"
+
+#ifndef dim             /*dim will be the dimension of calculations. If no calculation is defined, we set to 2*/
+#define dim 2
+#endif
+
+typedef struct site {
+
+
+}site;
