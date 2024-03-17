d5ff931336ef1830660265383794aca3a07add1a
FelixDedekind <png.spammail@gmail.com>
2024-03-17 19:57:16 +0100

    updated readme

--- standard/README.md
+++ standard/README.md
@@ -1,2 +1,4 @@
 # Potts-Model
 C code (maybe also python) to simulate Potts Model
+
+The files of interest are most likely just the simulation files, which are only the .c and .h files. C was used to simulate, whily python was used to plot.
--- standard/Tc_over_q.py
+++ standard/Tc_over_q.py
@@ -14,8 +14,10 @@ qspace = np.linspace(q[0]-1,q[-1]+1,1000)
 Tcspace = [1/(np.log(1+np.sqrt(qq))) for qq in qspace]
 
 plt.plot(qspace,Tcspace,label="$\mathrm{log}(1+\sqrt{\mathrm{q}})^{-1}$")
-plt.errorbar(q,Tc,yerr=Tc_error,capsize = 5,fmt = '.',label=r"$\mathrm{T_c}$")
+plt.errorbar(q,Tc,yerr=Tc_error,capsize = 5,fmt = '.',label=r"simulation")
 plt.xlim(q[0]-0.5,q[-1]+0.5)
+plt.xlabel("q")
+plt.ylabel(r"$\mathrm{k_BT_c/\epsilon}$")
 plt.grid()
 plt.legend()
 plt.show()
